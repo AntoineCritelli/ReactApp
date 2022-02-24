1c33f4dbfc3ed180f3d8c6f0c5397eefe6fca6cc
Antoine Critelli <critelli.antoine@gmail.com>
2022-02-24 15:49:00 +0100

    Ajout lien vers git et cv

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -4,8 +4,13 @@ import '../css/MainPage.css';
 class MainPage extends Component {
     render () {
         return (
-            <div>
-                main page
+            <div className="MainPage">
+                <p>
+                Ceci est un site démo React fait par <a href="/CV.pdf" target="_blank" className="lien">Antoine Critelli</a>
+                </p>
+                <p>
+                    Vous pouvez retrouver le code source sur mon <a href="https://github.com/AntoineCritelli/ReactApp" target="_blank" className="lien">git</a>.
+                </p>
             </div>
         )
     }
